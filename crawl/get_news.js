ad58027d1e940654f50e98a01e2f7b375a70a82f
Phong Do <phong.do@zaitenllc.com>
2021-01-29 15:38:43 +0700

    get news

---
+++ crawl/get_news.js
@@ -0,0 +1,129 @@
+const uuid = require('uuid');
+const Loader = require('./load');
+
+const domain = 'http://www.amthuc365.vn';
+const loader = new Loader();
+
+function loadPage(page) {
+    const url = `${domain}/che-bien-mon-ngon.html&page=${page}`;
+    return loader.load(url).then($ => {
+        const foods = [];
+        $('.it-new_bottom').each((index, item) => {
+            let food = {
+                id: uuid.v4()
+            };
+            food.url = domain + $(item).find('.it-img_bottom').find('a').attr('href');
+            food.img = $(item).find('.it-img_bottom').find('img').attr('src');
+            food.title = $(item).find('.it-body_bottom').find('h3').text().trim();
+            food.content = $(item).find('.it-body_bottom').find('p').text().trim();
+            foods.push(food);
+        });
+        return foods;
+    }).error(err => {
+        console.log(err);
+    });
+}
+
+function loadGroupPage(fromPage, toPage) {
+    const loadPromise = [];
+    for (let page = fromPage; page < toPage; page++) {
+        loadPromise.push(loadPage(page));
+    }
+    return Promise.all(loadPromise);
+}
+
+var foods = [];
+function loadAllPage(page, maxPage) {
+    if (!page) {
+        page = 1;
+    }
+    return loadGroupPage(page, page + 10).then(data => {
+        console.log(`${page} - ${page + 10}`);
+        data.forEach(item => {
+            foods.push(item);
+        })
+        page = page + 10;
+        if (page >= maxPage) {
+            return foods;
+        } else {
+            return loadAllPage(page, maxPage);
+        }
+    });
+}
+
+function loadNewsDetail(url) {
+    const data = [];
+    // `${domain}/${url}`
+    return loader.load(url).then($ => {
+        data.push({
+            type: 'text',
+            html: `<div>${$('.intro').html()}</div>`
+        });
+        // let content = $('.tinimce-content__new');
+        // if(content.children().length == 1){
+        //     content = $('.tinimce-content__new').children().first();
+        // }
+        let content = [];
+        if ($('.tinimce-content__new').find('table').length) {
+            return [];
+        }
+        $('.tinimce-content__new').children().each((index, item) => {
+            if ($(item).prop('tagName') == 'DIV') {
+                if (!$(item).children().length || $(item).children().length == 1) {
+                    content.push(item);
+                } else {
+                    $(item).children().each((index, item) => {
+                        content.push(item);
+                    });
+                }
+            } else {
+                content.push(item);
+            }
+        });
+        content.forEach((item, index) => {
+            if ($(item).html().indexOf('img') >= 0) {
+                data.push({
+                    type: 'image',
+                    src: $(item).find('img').attr('src')
+                })
+            } else {
+                data.push({
+                    type: 'text',
+                    html: `<${$(item).prop('tagName').toLowerCase()}>${$(item).html()}</${$(item).prop('tagName').toLowerCase()}>`
+                })
+            }
+        });
+        const groupHtml = [];
+        data.forEach(item => {
+            if (item.type == 'text') {
+                let currentItem = groupHtml[groupHtml.length - 1];
+                if (!currentItem || currentItem.type != 'text') {
+                    currentItem = {
+                        type: 'text',
+                        html: ''
+                    }
+                    groupHtml.push(currentItem);
+                }
+                currentItem.html += item.html;
+            }
+            if (item.type == 'image') {
+                let currentItem = {
+                    id: uuid.v4(),
+                    type: 'image',
+                    src: item.src
+                }
+                groupHtml.push(currentItem);
+            }
+        });
+        return groupHtml;
+    }).error(err => {
+        console.log(err);
+    });
+}
+
+// loadNewsDetail();
+
+module.exports = {
+    loadAllPage,
+    loadNewsDetail
+}
--- main.js
+++ main.js
@@ -56,3 +56,51 @@ const foods = require('./crawl/data/foods.json');
 //         console.log(err);
 //     })
 // })
+
+
+const getNews = require('./crawl/get_news');
+
+// getNews.loadAllPage(1, 45).then(data => {
+//     const foods = [];
+//     data.forEach(pageData => {
+//         pageData.forEach(item => {
+//             foods.push(item);
+//         });
+//     });
+//     // console.log(foods.length);
+//     fsExtra.writeJSON('./crawl/data/news.json', foods);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+const news = require('./crawl/data/news.json');
+// news.forEach((item, index) => {
+//     setTimeout(() => {
+//         var options = {
+//             directory: './crawl/data/news',
+//             filename: `${item.id}.jpg`
+//         }
+
+//         download(item.img, options, function (err) {
+//             if (err) {
+//                 console.log('ERROR - ' + item.img);
+//             }
+//         })
+//     }, Math.ceil(index / 5) * 1000);
+// });
+// const newsDetails = [];
+// news.forEach((item, index) => {
+//     setTimeout(() => {
+//         getNews.loadNewsDetail(item.url).then((data) => {
+//             if (data && data.length) {
+//                 newsDetails.push(data);
+//                 fsExtra.writeJSON(`./crawl/data/news-details/${item.id}.json`, data);
+//                 fsExtra.writeJSON(`./crawl/data/news-details.json`, newsDetails);
+//             }
+//         });
+//     }, Math.ceil(index / 2) * 1200);
+// });
+
+getNews.loadNewsDetail('http://www.amthuc365.vn/t9920c185/che-bien-mon-ngon/2011/12/diem-qua-nhung-mon-nem-o-viet-nam.html').then(data => {
+    console.log(data);
+})
